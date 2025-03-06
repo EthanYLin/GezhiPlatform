5439b7a84a0eacc887fec9683551844ffe5b9c28
Lin Yicheng <yichenglin20@fudan.edu.cn>
2025-03-06 13:48:42 +0800

    Initial Commit

---
+++ src/main/java/org/example/gezhiplatform/GezhiPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.gezhiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GezhiPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GezhiPlatformApplication.class, args);
+    }
+
+}
