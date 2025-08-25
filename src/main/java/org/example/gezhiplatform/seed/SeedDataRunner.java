fc8d52c777ed9ee817e29e87a068a2e4dbd17954
Lin Yicheng <yichenglin20@fudan.edu.cn>
2025-08-25 16:56:43 +0800

    feat: enhance SeedDataRunner to include user data generation and add UserFaker for user creation

--- src/main/java/org/example/gezhiplatform/seed/SeedDataRunner.java
+++ src/main/java/org/example/gezhiplatform/seed/SeedDataRunner.java
@@ -3,10 +3,14 @@ package org.example.gezhiplatform.seed;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.gezhiplatform.entity.GradeClass;
 import org.example.gezhiplatform.entity.Student;
+import org.example.gezhiplatform.entity.User;
 import org.example.gezhiplatform.entity.archive.Archive;
 import org.example.gezhiplatform.entity.enums.Campus;
+import org.example.gezhiplatform.entity.user_role.*;
 import org.example.gezhiplatform.repository.StudentRepository;
+import org.example.gezhiplatform.repository.UserRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -28,9 +32,14 @@ public class SeedDataRunner implements ApplicationRunner {
     private final ApplicationContext ctx;
     private final Scanner scanner = new Scanner(System.in);
     private final StudentRepository studentRepository;
+    private final UserRepository userRepository;
 
-    private record SeedStudents(int gradeNo, Campus campus, int classNo, int seatNoFrom, int seatNoTo){}
+    // ================ 模拟学生及档案数据 ================
+    // 2026级-2028级, 黄浦校区1-3班, 奉贤校区9班, 每班01~05号学生
+    private record SeedStudents(int gradeNo, Campus campus, int classNo, int seatNoFrom, int seatNoTo) {}
+
     private final List<SeedStudents> seedStudents = new ArrayList<>();
+
     @PostConstruct
     private void setSeedStudents() {
         for (int grade = 2026; grade <= 2028; grade++) {
@@ -44,37 +53,99 @@ public class SeedDataRunner implements ApplicationRunner {
     }
 
     private void generateStudentAndArchive() {
-        seedStudents.forEach(seedStudents -> {
-            IntStream.range(seedStudents.seatNoFrom(), seedStudents.seatNoTo() + 1)
-                .forEach(seatNo -> {
-                    int age = 2025 - seedStudents.gradeNo() + 18;
-                    var faker = PersonalInfoFaker.builder().age(age).build();
-                    Student student = StudentFaker.of(
-                        faker, seedStudents.campus(), seedStudents.gradeNo(), seedStudents.classNo(), seatNo
-                    );
-                    Archive archive = ArchiveFaker.of(faker);
-                    student.setArchive(archive);
-                    studentRepository.save(student);
-                });
-        });
+        seedStudents.forEach(seedStudents -> IntStream
+            .range(seedStudents.seatNoFrom(), seedStudents.seatNoTo() + 1)
+            .forEach(seatNo -> {
+                int age = 2025 - seedStudents.gradeNo() + 18;
+                var faker = PersonalInfoFaker.builder().age(age).build();
+                Student student = StudentFaker.of(
+                    faker, seedStudents.campus(), seedStudents.gradeNo(),
+                    seedStudents.classNo(), seatNo
+                );
+                Archive archive = ArchiveFaker.of(faker);
+                student.setArchive(archive);
+                studentRepository.save(student);
+            }));
+    }
+
+
+    // ================ 模拟用户数据 ================
+    // 1. 超级管理员 admin
+    // 2. 校级领导 school-leader
+    // 3. 年级组长 dean-2027 (2027届)
+    // 4. 班主任 class-2701 (2027届1班)
+    // 5. 班主任 class-2702 (2027届2班)
+    // 6. 多角色用户 multi-role (2028届年级组长、2027届3班班主任、260101及260102的协作用户)
+    // 7. 多班级观察员 mco (2028届1班、2028届2班)
+    // 8. 协作用户 cu-28xx01 (280101、280201、280301)
+    // 9. 年级组长+班主任 dual-2026-1 (2026届年级组长+2026届1班班主任)
+    private record SeedUser(String name, List<Role> roles) {}
+
+    private final List<SeedUser> seedUsers = new ArrayList<>();
+
+    @PostConstruct
+    private void setSeedUsers() {
+        seedUsers.add(new SeedUser("admin", List.of(new SuperAdmin())));
+        seedUsers.add(new SeedUser("school-leader", List.of(new Principal())));
+        seedUsers.add(new SeedUser("dean-2027", List.of(new GradeDean(2027))));
+        seedUsers.add(new SeedUser("class-2701", List.of(new ClassAdviser(new GradeClass(2027, 1)))));
+        seedUsers.add(new SeedUser("class-2702", List.of(new ClassAdviser(new GradeClass(2027, 2)))));
+        seedUsers.add(new SeedUser("multi-role", List.of(
+            new GradeDean(2028),
+            new ClassAdviser(new GradeClass(2027, 3)),
+            new CollaborativeUser(List.of("260101", "260102"))
+        )
+        ));
+        seedUsers.add(new SeedUser("mco", List.of(
+            new MultipleClassObserver(List.of(new GradeClass(2028, 1), new GradeClass(2028, 2)))
+        )
+        ));
+        seedUsers.add(new SeedUser("cu-28xx01", List.of(new CollaborativeUser(List.of("280101", "280201", "280301")))));
+        seedUsers.add(new SeedUser("dual-2026-1", List.of(
+            new GradeDean(2026),
+            new ClassAdviser(new GradeClass(2026, 1))
+        )
+        ));
+    }
+
+    private void generateUsers() {
+        List<User> seedUsers = this.seedUsers.stream().map(
+            seedUser ->
+                new UserFaker()
+                    .setName(seedUser.name())
+                    .setRoles(seedUser.roles())
+                    .sameUsernameAndName()
+                    .useDefaultPassword()
+                    .normalActiveAccount()
+                    .toUser()
+        ).toList();
+        userRepository.saveAll(seedUsers);
     }
 
     @Override
     public void run(ApplicationArguments args) {
         log.warn("你正在准备进行模拟数据生成...");
 
+        boolean isGenerateStudents = studentRepository.count() == 0;
+        boolean isGenerateUsers = userRepository.count() == 0;
         log.warn("1. 生成以下学生及档案");
-        log.warn(seedStudents.toString());
+        log.warn(isGenerateStudents ? seedStudents.toString() : "跳过(已存在学生数据)");
+        log.warn("2. 生成以下用户(初始密码均为: {} )", UserFaker.defaultPassword);
+        if (isGenerateUsers)
+            seedUsers.forEach(
+                su -> log.warn("{} -> {}", su.name(), su.roles().stream().map(Role::getRoleAndScope).toList()));
+        else
+            log.warn("跳过(已存在用户数据)");
 
-        System.out.println("输入[Y/y]继续...");
+        System.out.println("请确认要生成的数据是否正确, 并输入[Y/y]继续...");
         if (!scanner.nextLine().matches("[Yy]")) {
             log.warn("已取消数据生成。");
             return;
         }
 
-        generateStudentAndArchive();
-        System.out.println(studentRepository.count());
-        
+        if (isGenerateStudents) generateStudentAndArchive();
+        if (isGenerateUsers) generateUsers();
+
         System.exit(SpringApplication.exit(ctx));
     }
 
---
+++ src/main/java/org/example/gezhiplatform/seed/UserFaker.java
@@ -0,0 +1,76 @@
+package org.example.gezhiplatform.seed;
+
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.example.gezhiplatform.entity.User;
+import org.example.gezhiplatform.entity.user_role.Role;
+import org.example.gezhiplatform.utils.PasswordEncryptUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Accessors(chain = true)
+public class UserFaker {
+
+    public static String defaultPassword = "123456";
+
+    @Setter @Nullable private Long id;
+    @Setter @Nullable private String name;
+    @Setter @Nullable private String username;
+    @Setter @Nullable private String rawPassword;
+    @Setter @Nullable private Boolean locked;
+    @Setter @Nullable private Boolean enabled;
+    @Setter @Nullable private LocalDateTime lastLoginTime;
+    @Setter @Nullable private List<Role> roles;
+
+    @Nullable private Boolean isSameUsernameAndName;
+    @Nullable private Boolean isUseDefaultPassword;
+    @Nullable private Boolean isNormalActiveAccount;
+
+    public UserFaker sameUsernameAndName() {
+        this.isSameUsernameAndName = true;
+        return this;
+    }
+
+    public UserFaker useDefaultPassword() {
+        this.isUseDefaultPassword = true;
+        return this;
+    }
+
+    public UserFaker normalActiveAccount() {
+        this.isNormalActiveAccount = true;
+        return this;
+    }
+
+    public User toUser() {
+        User user = new User();
+        user.setId(id);
+        if (Boolean.TRUE.equals(isSameUsernameAndName) && name != null) {
+            user.setUsername(name);
+            user.setName(name);
+        } else if (Boolean.TRUE.equals(isSameUsernameAndName) && username != null) {
+            user.setUsername(username);
+            user.setName(username);
+        } else {
+            user.setUsername(username);
+            user.setName(name);
+        }
+        if (Boolean.TRUE.equals(isUseDefaultPassword)) {
+            user.setEncryptedPassword(PasswordEncryptUtils.encode(defaultPassword));
+        } else if (rawPassword != null) {
+            user.setEncryptedPassword(PasswordEncryptUtils.encode(rawPassword));
+        }
+        if (Boolean.TRUE.equals(isNormalActiveAccount)) {
+            user.setLocked(false);
+            user.setEnabled(true);
+        } else {
+            if (locked != null) user.setLocked(locked);
+            if (enabled != null) user.setEnabled(enabled);
+        }
+        user.setLastLoginTime(lastLoginTime);
+        if (roles != null) user.getRoles().addAll(roles);
+        return user;
+    }
+
+}
