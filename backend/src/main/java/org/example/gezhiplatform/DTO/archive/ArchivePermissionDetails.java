b828fda0781331755ecc82d47627a5429debd3e5
Lin Yicheng <yichenglin20@fudan.edu.cn>
2025-11-24 14:59:24 +0800

    feat: 添加 displayCaption 字段以合并权限组公告信息

--- backend/src/main/java/org/example/gezhiplatform/DTO/archive/ArchivePermissionDetails.java
+++ backend/src/main/java/org/example/gezhiplatform/DTO/archive/ArchivePermissionDetails.java
@@ -3,8 +3,10 @@ package org.example.gezhiplatform.DTO.archive;
 import jakarta.validation.constraints.NotNull;
 import org.example.gezhiplatform.entity.archive.PermissionGroup;
 import org.example.gezhiplatform.entity.archive.ValidationExpr;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
  *
  * @param grantedRoleAndScopes 用户拥有的且可访问该档案的角色范围列表
  * @param ownedPermissionGroups 用户拥有的且可访问该档案的权限组名称列表
+ * @param displayCaption 要显示的公告(所有权限组的公告合并而成，若无公告则为null)
  * @param allowedReadableJsonPaths 允许读取的字段JSON Path集合
  * @param allowedWritableJsonPaths 允许写入的字段JSON Path集合
  * @param allowedAddArrayJsonPaths 允许添加数组元素的JSON Path集合
@@ -27,6 +30,7 @@ import java.util.stream.Collectors;
 public record ArchivePermissionDetails(
     @NotNull List<String> grantedRoleAndScopes,
     @NotNull List<String> ownedPermissionGroups,
+    @Nullable String displayCaption,
     @NotNull Set<String> allowedReadableJsonPaths,
     @NotNull Set<String> allowedWritableJsonPaths,
     @NotNull Set<String> allowedAddArrayJsonPaths,
@@ -56,6 +60,10 @@ public record ArchivePermissionDetails(
         @NotNull Set<PermissionGroup> ownedGroups,
         @NotNull List<String> grantedRoleAndScopes
     ) {
+
+        String displayCaption = ownedGroups.stream()
+            .map(PermissionGroup::getDisplayCaption).filter(Objects::nonNull).collect(Collectors.joining("\n"));
+
         Set<String> allowedReadableJsonPaths = ownedGroups.stream()
             .flatMap(group -> group.getAllowedReadableJsonPaths().stream())
             .collect(Collectors.toSet());
@@ -87,6 +95,7 @@ public record ArchivePermissionDetails(
         return new ArchivePermissionDetails(
             grantedRoleAndScopes,
             ownedPermissionGroups,
+            displayCaption,
             allowedReadableJsonPaths,
             allowedWritableJsonPaths,
             allowedAddArrayJsonPaths,
